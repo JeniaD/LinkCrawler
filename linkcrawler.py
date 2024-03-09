c74ef73c754a19091a1c8ceacc6e3570337d6416
JeniaD <jeniadeshchenko@gmail.com>
2024-03-09 23:55:52 +0100

    Added URL cleaning and --filter

--- linkcrawler.py
+++ linkcrawler.py
@@ -4,10 +4,22 @@ from bs4 import BeautifulSoup
 
 RESULTS = set()
 
-def CheckURL(url):
-    if not url.split("/")[0] or url.split("/")[0] == '.': return False
+def CheckURL(url, topDomain=None):
+    if not url.split('/')[0] or url.split('/')[0] == '.' or '.' not in url or "mailto:" in url: return False
+    if topDomain and topDomain not in url: return False
 
     return True
+
+def CleanURL(url):
+    url = url.replace("www.", '') # WARNING: might fail opening some sites without redirect
+
+    if "http" not in url:
+        return url.split('/')[0].split('?')[0]
+    else:
+        if "https" in url:
+            return "https://" + url.replace("https://", '').split('/')[0].split('?')[0]
+        else:
+            return "http://" + url.replace("http://", '').split('/')[0].split('?')[0]
 
 def Parse(url):
     try:
@@ -20,33 +32,35 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=f"Simple link web crawler")
     parser.add_argument("target", help="your target URL")
     # parser.add_argument("--output", help="output directory path")
-    # parser.add_argument("--filter", help="domain allowed to crawl (like .com)")
+    parser.add_argument("--filter", help="domain allowed to crawl (like .com)")
     args = parser.parse_args()
 
-    # root = Parse(args.target)
-    # if not root: exit(0)
     RESULTS.add(args.target)
     toScan = [args.target]
     newLinks = []
     
-    while True:
-        for link in toScan:
-            try:
-                soup = BeautifulSoup(Parse(link).content, "html.parser")
+    try:
+        while True:
+            for link in toScan:
+                try:
+                    soup = BeautifulSoup(Parse(link).content, "html.parser")
 
-                linkElements = soup.select("a[href]")
-                for elem in linkElements:
-                    url = elem["href"]
-                    if CheckURL(url) and url not in newLinks and url not in toScan and url not in RESULTS:
-                        newLinks += [url]
-                        print(url)
-                    
-                    RESULTS.add(url)
-            except AttributeError as e:
-                continue
-        
-        if not newLinks: break
-        else:
-            for elem in toScan: RESULTS.add(elem)
-            toScan = newLinks
-            newLinks = []
+                    linkElements = soup.select("a[href]")
+                    for elem in linkElements:
+                        url = CleanURL(elem["href"])
+                        if CheckURL(url, args.filter) and url not in newLinks and url not in toScan and url not in RESULTS:
+                            newLinks += [url]
+                            print(url)
+                        
+                        RESULTS.add(url)
+                except AttributeError as e:
+                    continue
+            
+            if not newLinks: break
+            else:
+                for elem in toScan: RESULTS.add(elem)
+                toScan = newLinks
+                newLinks = []
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+        print(RESULTS)
